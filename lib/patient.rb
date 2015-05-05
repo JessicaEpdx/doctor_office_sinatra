37a4299154efa13445924f654600f85da312406c
steve schneider & jessica engel <student@epicodus.com>
2015-05-05 16:42:55 -0700

    add layout pages, success pages. add methods for patients.

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,4 +11,16 @@ attr_reader(:name, :birthday, :doctor_id)
     result = DB.exec("INSERT INTO patients (name, birthday, doctor_id) VALUES ('#{@name}', '#{@birthday}', #{@doctor_id});")
   end
 
+  define_singleton_method(:all) do
+    all_pats = DB.exec("SELECT * FROM patients;")
+    result = []
+    all_pats.each() do |pat|
+      name = pat.fetch('name')
+      birthday = pat.fetch('birthday')
+      doctor_id = pat.fetch('doctor_id').to_i
+      result.push(Patient.new({:name => name, :birthday => birthday, :doctor_id => doctor_id}))
+    end
+    result
+  end
+
 end
