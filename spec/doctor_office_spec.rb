8247b7283e7b7ecd641269fc369aefab4a262fa2
steve schneider & jessica engel <student@epicodus.com>
2015-05-05 17:24:21 -0700

    start styling

--- spec/doctor_office_spec.rb
+++ spec/doctor_office_spec.rb
@@ -8,7 +8,7 @@ describe('going to the doctors list', {:type => :feature}) do
   it('allows a user to see a list of doctors') do
     visit('/')
     click_link('See all your damn doctors!')
-    expect(page).to have_content("These are your DOCTORS!")
+    expect(page).to have_content("All Doctors")
   end
 end
 
@@ -17,7 +17,7 @@ describe('add doctor form path', {:type => :feature}) do
     visit('/')
     fill_in('doc', :with => 'Dr. Dre')
     fill_in('specialty', :with => 'Phat Beatz')
-    click_button('ADD THAT DOCTORRRRR')
+    click_button('Add')
     expect(page).to have_content('Dr. Dre')
   end
 end
